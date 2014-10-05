87617f2bb051cf8aea7d4c0ffc272fca9f379fc6
tbrockman <tbrockma@ualberta.ca>
2014-10-05 13:41:59 -0600

    bloopers

--- cmput301t03appTEST-test/gen/theoTest.java
+++ cmput301t03appTEST-test/gen/theoTest.java
@@ -12,7 +12,16 @@ import ca.ualberta.cs.cmput301t03app.MainActivity;
 
 public class theoTest extends ActivityInstrumentationTestCase2<MainActivity>
 {
+	// Creates a list of questions and and list of answers
+	// iterates through all existing questions and answers, favoriting
+	// each question and answer and then adding them to the
+	// question and answer lists.
+	// It then uses the getter methods for favourites answers
+	// and questions and then compares the two lists
+	// which should be equal
+	
 	public void testViewFavorites() {
+		
 		ArrayList<Question> favQuestions = new ArrayList<Question>();
 		ArrayList<Answer> favAnswer = new ArrayList<Answer>();
 		forumQuestionList listTest = new forumQuestionList;
@@ -32,17 +41,31 @@ public class theoTest extends ActivityInstrumentationTestCase2<MainActivity>
 		assertEquals(favAnswers,a);
 	}
 	
+	// Creates a dummy questions, adds an upvote
+	// asserts that the question has one upvote
+	
 	public void testUpvoteQuestions() {
+		
 		Question testQuestion = new Question;
-		testQuestions.upvote();
+		testQuestions.changeRating();
 		assertEquals(testQuestion.getRating(),1);
 	}
 	
+	// Creates dummy answer, adds and upvote
+	// asserts that the question has one upvote
+	
 	public void testUpvoteAnswers() {
+		
 		Answer testAnswer = new Answer;
-		testAnswers.upvote();
+		testAnswers.changeRating();
 		assertEquals(testAnswer.getRating(), 1);
 	}
+	
+	// Creates a dummy author, uses a dummy function
+	// of uploading a test jpg file, sets the author profile
+	// picture to the newly uploaded file.
+	// Asserts with a getter method that the jpg returned and
+	// the jpg uploaded are the same.
 	
 	public void testProfilePic() {
 		Author newAuthor = new Author;
