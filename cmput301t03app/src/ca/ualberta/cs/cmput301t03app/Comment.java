a4b39b06e0a06460b0215787c5b9e637451d86c9
jivedesign <jhn@ualbeta.ca>
2014-10-05 14:25:33 -0600

    Adding testcases for comments

---
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/Comment.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.cmput301t03app;
+
+import java.sql.Date;
+
+public class Comment {
+	/*
+	 * This is the comment class. It allows users to set the text of a comment*/
+		
+	public Date dateCreated;
+	public String textBody;
+	
+	
+	public void setTextBody(String comment) {
+		textBody = comment;
+			
+	}
+	
+	
+}
