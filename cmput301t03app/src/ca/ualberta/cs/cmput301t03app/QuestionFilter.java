83440e072a064b37ad6979ac5113fc9bddcd9271
Dominik <pachala@ualberta.ca>
2014-10-16 00:21:58 -0600

    Added activities

--- cmput301t03app/src/ca/ualberta/cs/cmput301t03app/QuestionFilter.java
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/QuestionFilter.java
@@ -1,5 +1,13 @@
 package ca.ualberta.cs.cmput301t03app;
 
 public class QuestionFilter {
-
+	public Object sortByUpvote(){
+		return null;
+	}
+	public Object sortByDate(){
+		return null;
+	}
+	public Object sortByPic(){
+		return null;
+	}
 }
