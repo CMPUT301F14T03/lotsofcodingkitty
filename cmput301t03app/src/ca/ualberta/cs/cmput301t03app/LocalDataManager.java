7b999748ddcccc6198cd4da804374acca9440dd0
rjynn <rdejesus@cs.ualberta.ca>
2014-10-20 13:28:01 -0600

    added new datamanager interface and two classes

---
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/LocalDataManager.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.cmput301t03app;
+
+import java.util.ArrayList;
+
+public class LocalDataManager implements iDataManager{
+
+	@Override
+	public void save(ArrayList<Question> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
