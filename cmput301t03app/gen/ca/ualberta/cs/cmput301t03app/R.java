5a5c30247721dd69e71b26d5ec35ee614639a010
jivedesign <josh.nguyen88@gmail.com>
2014-10-22 10:44:21 -0600

    Added more icons. Tweaks to leading and kerning. Tasteful icon sizes. Design focus.

--- cmput301t03app/gen/ca/ualberta/cs/cmput301t03app/R.java
+++ cmput301t03app/gen/ca/ualberta/cs/cmput301t03app/R.java
@@ -29,12 +29,14 @@ public final class R {
         public static final int camera_icon=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_fav_no=0x7f020002;
-        public static final int ic_fav_yes=0x7f020003;
-        public static final int ic_filter=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int ic_sync=0x7f020006;
-        public static final int ic_upvote=0x7f020007;
-        public static final int ic_user=0x7f020008;
+        public static final int ic_fav_no__blue_small=0x7f020003;
+        public static final int ic_fav_yes=0x7f020004;
+        public static final int ic_fav_yes_blue_small=0x7f020005;
+        public static final int ic_filter=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int ic_sync=0x7f020008;
+        public static final int ic_upvote=0x7f020009;
+        public static final int ic_user=0x7f02000a;
     }
     public static final class id {
         public static final int UsernameRespondTextView=0x7f08000d;
@@ -47,6 +49,10 @@ public final class R {
         public static final int attachImgTxt=0x7f08000f;
         public static final int doneBtn=0x7f080000;
         public static final int filter=0x7f080018;
+        public static final int filter_date=0x7f08001b;
+        public static final int filter_newest=0x7f08001a;
+        public static final int filter_picture=0x7f08001c;
+        public static final int filter_popular=0x7f080019;
         public static final int inputUsername=0x7f080001;
         public static final int listView1=0x7f080011;
         public static final int post_timestamp=0x7f080009;
@@ -60,7 +66,7 @@ public final class R {
         public static final int question_upvote_score=0x7f080005;
         public static final int question_viewed_icon=0x7f080007;
         public static final int search=0x7f080017;
-        public static final int sync=0x7f080019;
+        public static final int sync=0x7f08001d;
         public static final int user=0x7f080016;
         public static final int usernameText=0x7f080002;
     }
