98a43ddac6d03c28248ed3f7f504a61d37bc1e2c
jivedesign <josh.nguyen88@gmail.com>
2014-10-21 21:33:31 -0600

    changed the icons in view question activity xml's

--- cmput301t03app/gen/ca/ualberta/cs/cmput301t03app/R.java
+++ cmput301t03app/gen/ca/ualberta/cs/cmput301t03app/R.java
@@ -27,7 +27,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int camera_icon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_fav_no=0x7f020002;
+        public static final int ic_fav_yes=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_upvote=0x7f020005;
+        public static final int ic_user=0x7f020006;
     }
     public static final class id {
         public static final int CancelButton=0x7f08000f;
