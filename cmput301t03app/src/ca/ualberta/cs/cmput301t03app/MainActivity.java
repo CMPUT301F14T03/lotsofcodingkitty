96a923c0715d5a414d3ae2a9486ad6e587df5b3b
jivedesign <josh.nguyen88@gmail.com>
2014-10-26 13:09:50 -0600

    removed a line of unfinished code

--- cmput301t03app/src/ca/ualberta/cs/cmput301t03app/MainActivity.java
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/MainActivity.java
@@ -100,11 +100,6 @@ public class MainActivity extends Activity {
 	}
 
 	
-	public void setOnClickListener(MainActivity) {
-		
-		
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
