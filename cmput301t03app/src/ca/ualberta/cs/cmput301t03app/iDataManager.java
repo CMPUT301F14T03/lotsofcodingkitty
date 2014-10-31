83ad17b0f64ce96ef21c82121fd76847b559bc9d
cchan5 <ckchan@ualberta.ca>
2014-10-30 22:03:19 -0600

    Got rid of appending functionality

--- cmput301t03app/src/ca/ualberta/cs/cmput301t03app/iDataManager.java
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/iDataManager.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public interface iDataManager {
 
-	public void appendToQuestionBank(Question q);
+//	public void appendToQuestionBank(Question q);
 	public void saveToQuestionBank(ArrayList<Question> list);
 	public ArrayList<Question> loadQuestions();
 	
-	public void saveAnswer(Answer a);
+//	public void saveAnswer(Answer a);
 	public void saveAnswers(ArrayList<Answer> list);
 	
 //	void saveAnswers(ArrayList<Answer> list);
