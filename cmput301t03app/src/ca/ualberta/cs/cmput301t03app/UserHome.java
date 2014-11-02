2e89ed01ef0fe2264cca35af7059c9c850afaec2
jivedesign <josh.nguyen88@gmail.com>
2014-11-02 10:50:29 -0700

    Josh: made user Home and fixed up a lot more stuff

---
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/UserHome.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.cmput301t03app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserHome extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_user_home);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
