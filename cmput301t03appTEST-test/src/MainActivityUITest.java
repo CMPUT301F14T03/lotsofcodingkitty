eaa1f612d2bf6459cf6ca3b4995d5115a5b77284
rjynn <rdejesus@cs.ualberta.ca>
2014-11-02 15:29:32 -0700

    added some comments to testing

--- cmput301t03appTEST-test/src/MainActivityUITest.java
+++ cmput301t03appTEST-test/src/MainActivityUITest.java
@@ -52,18 +52,18 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
 	public void makeQuestion(String title, String body, String name){	//This is used for test that makes a question
 		assertNotNull("this should be a button",activity.findViewById(ca.ualberta.cs.cmput301t03app.R.id.activity_main_question_button));
 		((Button) activity.findViewById(ca.ualberta.cs.cmput301t03app.R.id.activity_main_question_button)).performClick();
-	    AlertDialog dialog = activity.getDialog();
-	     EditText questionTitle = (EditText) 
+	    AlertDialog dialog = activity.getDialog();	//grabbing the dialog box that opens up when button clicked	
+	     EditText questionTitle = (EditText) 		//grabbing all edit texts in teh dialog box
 				dialog.findViewById(R.id.questionTitle);
 		 EditText questionBody = (EditText) 
 				dialog.findViewById(R.id.questionBody);
 		 EditText userName = (EditText) 
 				dialog.findViewById(R.id.UsernameRespondTextView);
-		questionTitle.setText(title);
+		questionTitle.setText(title);	//setting them up with arguments
 		questionBody.setText(body);
 		userName.setText(name);
 		dialog.getButton(
-				 DialogInterface.BUTTON_POSITIVE).performClick();
+				 DialogInterface.BUTTON_POSITIVE).performClick();	//clicking to add the question
 		
 	
 	}
