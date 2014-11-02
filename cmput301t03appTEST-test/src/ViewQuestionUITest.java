e626fe11307f361aa6aeb1621639eb9308e8231d
tbrockman <tbrockma@ualberta.ca>
2014-11-02 12:50:22 -0700

    remove upc object from viewquestion

---
+++ cmput301t03appTEST-test/src/ViewQuestionUITest.java
@@ -0,0 +1,26 @@
+import ca.ualberta.cs.cmput301t03app.ViewQuestion;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+
+public class ViewQuestionUITest extends ActivityInstrumentationTestCase2<ViewQuestion>
+{
+	Instrumentation instrumentation;
+	Activity activity;
+	
+	public ViewQuestionUITest(Class<ViewQuestion> activityClass)
+	{
+
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		
+	
+	}
+	
+	
+}
