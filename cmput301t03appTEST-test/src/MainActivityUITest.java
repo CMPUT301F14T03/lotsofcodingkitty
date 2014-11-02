e8e674b70fd22acb58c2e2c2e2e744b257197387
rjynn <rdejesus@cs.ualberta.ca>
2014-11-02 15:17:40 -0700

    UI test WORKS :D

--- cmput301t03appTEST-test/src/MainActivityUITest.java
+++ cmput301t03appTEST-test/src/MainActivityUITest.java
@@ -50,21 +50,22 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
 	
 	@UiThreadTest
 	public void makeQuestion(String title, String body, String name){	//This is used for test that makes a question
+		assertNotNull("this should be a button",activity.findViewById(ca.ualberta.cs.cmput301t03app.R.id.activity_main_question_button));
 		((Button) activity.findViewById(ca.ualberta.cs.cmput301t03app.R.id.activity_main_question_button)).performClick();
 	    AlertDialog dialog = activity.getDialog();
-		final EditText questionTitle = (EditText) 
-				activity.findViewById(R.id.questionTitle);
-		final EditText questionBody = (EditText) 
-				activity.findViewById(R.id.questionBody);
-		final EditText userName = (EditText) 
-				activity.findViewById(R.id.UsernameRespondTextView);
+	     EditText questionTitle = (EditText) 
+				dialog.findViewById(R.id.questionTitle);
+		 EditText questionBody = (EditText) 
+				dialog.findViewById(R.id.questionBody);
+		 EditText userName = (EditText) 
+				dialog.findViewById(R.id.UsernameRespondTextView);
 		questionTitle.setText(title);
 		questionBody.setText(body);
 		userName.setText(name);
 		dialog.getButton(
 				 DialogInterface.BUTTON_POSITIVE).performClick();
 		
-		
+	
 	}
 	
 	@UiThreadTest
@@ -73,7 +74,8 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
 		MainListAdapter adapter = activity.getAdapter();
 		int oldCount = adapter.getCount();
 		makeQuestion("why?","why?","rjynn");
-		assertEquals("new question added", oldCount, oldCount+1); 
+		int newCount = adapter.getCount();
+		assertEquals("new question added", newCount, oldCount); 
 
 	}
 	public void testListView(){
@@ -90,3 +92,5 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
 	
 	
 }
+
+
