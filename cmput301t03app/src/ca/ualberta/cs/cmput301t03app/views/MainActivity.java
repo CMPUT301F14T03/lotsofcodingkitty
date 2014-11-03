f9b08c38387540086fe516718ffecbb0046705d6
rjynn <rdejesus@cs.ualberta.ca>
2014-11-03 16:16:18 -0700

    made MainActivity easier to read by fixing style errors and warnings

--- cmput301t03app/src/ca/ualberta/cs/cmput301t03app/views/MainActivity.java
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/views/MainActivity.java
@@ -1,8 +1,12 @@
-
+/*
+ * This class is the main access point to the application. 
+ * 
+ * @author
+ * 
+ * */
 package ca.ualberta.cs.cmput301t03app.views;
 
 import java.util.ArrayList;
-
 import ca.ualberta.cs.cmput301t03app.R;
 import ca.ualberta.cs.cmput301t03app.R.id;
 import ca.ualberta.cs.cmput301t03app.R.layout;
@@ -33,14 +37,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-/*
- * This class is the main access point to the application. 
- * 
- * @author
- * 
- * */
-
-
 public class MainActivity extends Activity {
 	ListView lv;
 	MainListAdapter mla;
@@ -52,11 +48,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		ListView questionList = (ListView) findViewById(R.id.activity_main_question_list);
-
 		questionList.setOnItemClickListener(new OnItemClickListener() {
-
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					final int position, long id) {
@@ -65,7 +58,6 @@ public class MainActivity extends Activity {
 				Log.d("click", "click " + position);
 				
 				toQuestionActivity(position);
-				
 			}
 		});
 		
@@ -104,9 +96,7 @@ public class MainActivity extends Activity {
 	public void onResume() {
 		super.onResume();
 		mla.updateAdapter(pc.getQuestionsInstance());
-		
 	}
-
 
 	public void setupAdapter() {
 		lv = (ListView) findViewById(R.id.activity_main_question_list);
@@ -116,7 +106,6 @@ public class MainActivity extends Activity {
 	}
 
 	public void toQuestionActivity(int position) {
-		
 		Intent i = new Intent( this, ViewQuestion.class );
 		i.putExtra("question_id", pc.getQuestionsInstance().get(position).getId());
 		pc.addReadQuestion(pc.getQuestionsInstance().get(position));
@@ -136,48 +125,32 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		
+		if (id == R.id.action_settings) {return true;}
 		if (id == R.id.user_home) {
 			Intent intent = new Intent(this, UserHome.class);
 			startActivity(intent);
 		}
-		
 		return super.onOptionsItemSelected(item);
 	}
 
 	public void addQuestionButtonFunction(View view) {
 		// Pops up dialog box for adding a question
-
 		LayoutInflater li = LayoutInflater.from(this);
-
-		// Get XML file to view
-		View promptsView = li.inflate(R.layout.activity_respond, null);
-
+		View promptsView = li.inflate(R.layout.activity_respond, null);// Get XML file to view
 		final EditText questionTitle = (EditText) promptsView
 				.findViewById(R.id.questionTitle);
-
 		final EditText questionBody = (EditText) promptsView
 				.findViewById(R.id.questionBody);
-
 		final EditText userName = (EditText) promptsView
 				.findViewById(R.id.UsernameRespondTextView);
+		
+		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);// Create a new AlertDialog
 
-		// Intent intent = new Intent(this, Respond.class);
-		// startActivity(intent);
-
-		// Create a new AlertDialog
-		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-
-		// Link the alertdialog to the XML
-		alertDialogBuilder.setView(promptsView);
-
-		// Building the dialog for adding
+		
+		alertDialogBuilder.setView(promptsView);// Link the alertdialog to the XML
 		alertDialogBuilder.setPositiveButton("Ask!",
 				new DialogInterface.OnClickListener() {
-					@Override
+					@Override// Building the dialog for adding
 					public void onClick(DialogInterface dialog, int which) {
 
 						String questionTitleString = (String) questionTitle
@@ -210,16 +183,14 @@ public class MainActivity extends Activity {
 					}
 				});
 		
-				
-
 		final AlertDialog alertDialog = alertDialogBuilder.create();
 		alertDialog1 = alertDialog;
 		alertDialog.show();
 		alertDialog.getButton(AlertDialog.BUTTON1).setEnabled(false);
 		
-		
-		//creating a listener to see if any changes to edit text in dialog
-		TextWatcher textwatcher = new TextWatcher(){
+
+		TextWatcher textwatcher = new TextWatcher(){		
+			//creating a listener to see if any changes to edit text in dialog
 			private void handleText(){
 				final Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
 				if(questionTitle.getText().length() == 0){	//these checks the edittext to make sure not empty edit text
@@ -237,24 +208,20 @@ public class MainActivity extends Activity {
 			}
 			@Override
 			public void afterTextChanged(Editable s) {
-				// TODO Auto-generated method stub
 				handleText();
 			}
 			@Override
 			public void beforeTextChanged(CharSequence s, int start,
 					int count, int after) {
-				// TODO Auto-generated method stub
-				
+				//do nothing
 			}
 			@Override
 			public void onTextChanged(CharSequence s, int start,
 					int before, int count) {
-				// TODO Auto-generated method stub
-				
+				//do nothing
 			}
 		};
 			
-		
 		questionTitle.addTextChangedListener(textwatcher);	//adding listeners to the edittexts
 		questionBody.addTextChangedListener(textwatcher);
 		userName.addTextChangedListener(textwatcher);
@@ -262,40 +229,31 @@ public class MainActivity extends Activity {
 				.show();
 	}
 	
-	
 	public MainListAdapter getAdapter(){
 		return mla;
 	}
 	
-	
-public void addToToRead(final int position) {
-		
+	public void addToToRead(final int position) {
 		// Brings up DIalog window with options to Archive and Delete
 		// http://developer.android.com/reference/android/app/AlertDialog.Builder.html#setSingleChoiceItems(java.lang.CharSequence[],
 		// int, android.content.DialogInterface.OnClickListener)
 		// Edit existing todo with LONG CLICK
 
-		LayoutInflater inflator = LayoutInflater.from(this);
-
-		View editTodo = inflator.inflate(R.layout.dialog_to_read, null);
-
 		AlertDialog.Builder editDialog = new AlertDialog.Builder(this);
-
 		editDialog
-				.setNegativeButton("Cancel",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								dialog.cancel();
-							}
-						})
-				.setPositiveButton("Add to To-Read",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								pc.addToRead(pc.getQuestionsInstance().get(position));
-							}
-						});
-
-
+			.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				})
+			.setPositiveButton("Add to To-Read",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						pc.addToRead(pc.getQuestionsInstance().get(position));
+					}
+				});
+		
 		AlertDialog alertDialog = editDialog.create();
 		alertDialog.show();
 
