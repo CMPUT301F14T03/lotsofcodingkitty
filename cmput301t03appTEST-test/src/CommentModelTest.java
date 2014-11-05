23731f171ccd21122fd0d13b718335b926471f01
pachalad <pachala@ualberta.ca>
2014-11-04 17:50:17 -0700

    Added another test for the comment model. This should be fine now.

--- cmput301t03appTEST-test/src/CommentModelTest.java
+++ cmput301t03appTEST-test/src/CommentModelTest.java
@@ -1,6 +1,8 @@
+import java.util.Date;
+
+import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.cs.cmput301t03app.models.Comment;
 import ca.ualberta.cs.cmput301t03app.views.MainActivity;
-import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * This test suite is used to test the functionalities of the Comment object
@@ -16,7 +18,22 @@ public class CommentModelTest extends ActivityInstrumentationTestCase2<MainActiv
 	// Checks to ensure that the constructor sets attributes correctly by using the retrieval methods
 	public void testCommentConstructor()  {
 		Comment c1 = new Comment("a comment", "author");
+		Date date= new Date();
 		assertEquals("Comment made is not the expected string",  "a comment", c1.getCommentBody());
+		assertEquals("Comment has not the expected author","author",c1.getAuthor());
+		assertEquals("Dates are not the same",date,c1.getDate());
+	}
+	// Creates two comments and makes sure they aren't the same. 
+	public void testTwoDifferentComments(){
+		Comment c = new Comment("comment1","author1");
+		Comment c1 = new Comment("comment2","author2");
+		
+		assertFalse("The items are the same", c==c1);
+		assertFalse("The authors are the same for both comments",c.getAuthor().equals(c1.getAuthor()));
+		assertFalse("The comments are the same for both comments", c.getCommentBody().equals(c1.getCommentBody()));
+		assertFalse("The dates are not the same",c.getDate()==c1.getDate());
+		
+		
 	}
 	
 }
