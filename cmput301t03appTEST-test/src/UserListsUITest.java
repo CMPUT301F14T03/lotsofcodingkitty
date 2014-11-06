58d769a4bde8daa277232f2535a1ac045be9c6e9
rjynn <rdejesus@ualberta.ca>
2014-11-06 16:02:39 -0700

    added userListsUITest

--- cmput301t03appTEST-test/src/UserListsUITest.java
+++ cmput301t03appTEST-test/src/UserListsUITest.java
@@ -1,73 +1,47 @@
 import ca.ualberta.cs.cmput301t03app.R;
+import ca.ualberta.cs.cmput301t03app.models.Question;
 import ca.ualberta.cs.cmput301t03app.views.UserListsActivity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class UserListsUITest extends ActivityInstrumentationTestCase2<UserListsActivity> {
-
+	Instrumentation instru;
 	public UserListsUITest() {
 		super(UserListsActivity.class);
 		
 	}
-	
-	public void testItemsOnList(){
-			UserListsActivity activity = (UserListsActivity) getActivity();
-			ListView answerListView;
-			ImageButton favIcon;
-			ImageButton upvoteButton;
-			ImageButton commentButton;
-			TextView upvote_score;
-			Button answerButton;
-			TextView answerCounter;
-			TextView commentCounter;
 
-			answerListView = (ListView) activity.findViewById(R.id.answerListView);
-			favIcon = (ImageButton) activity.findViewById(R.id.question_fav_icon);
-			upvoteButton = (ImageButton) activity
-					.findViewById(R.id.question_upvote_button);
-			commentButton = (ImageButton) activity
-					.findViewById(R.id.question_comment_icon);
-			upvote_score = (TextView) activity
-					.findViewById(R.id.question_upvote_score);
-			answerButton = (Button) activity
-					.findViewById(R.id.question_answer_button);
-			answerCounter = (TextView) activity.findViewById(R.id.answer_count);
-			commentCounter = (TextView) activity
-					.findViewById(R.id.question_comment_count);
-			answerListView = (ListView) activity.findViewById(R.id.answerListView);
+	public void testItemsOnList(){
+			instru = getInstrumentation();
+			UserListsActivity activity = (UserListsActivity) getActivity();
+			ListView userListView;
+			TextView title;
+
+			userListView = (ListView) activity.findViewById(R.id.user_question_list);
+			title = (TextView) activity.findViewById(R.id.user_list_title);
 
 			// Assert that none of the views are null
 
-			assertNotNull("Item not created for question view", answerListView);
-			assertNotNull("Item not created for question view", favIcon);
-			assertNotNull("Item not created for question view", upvoteButton);
-			assertNotNull("Item not created for question view", commentButton);
-			assertNotNull("Item not created for question view", upvote_score);
-			assertNotNull("Item not created for question view", answerCounter);
-			assertNotNull("Item not created for question view", answerButton);
-			assertNotNull("Item not created for question view", commentCounter);
-
+			assertNotNull("Item not created for question view", userListView);
+			assertNotNull("Item not created for question view", title);
+			
 			View mainView = (View) activity.getWindow().getDecorView()
 					.findViewById(android.R.id.content);
 
 			// Assert that all of the views are displayed on screen
-
-			ViewAsserts.assertOnScreen(mainView, answerListView);
-			ViewAsserts.assertOnScreen(mainView, favIcon);
-			ViewAsserts.assertOnScreen(mainView, upvoteButton);
-			ViewAsserts.assertOnScreen(mainView, commentButton);
-			ViewAsserts.assertOnScreen(mainView, upvote_score);
-			ViewAsserts.assertOnScreen(mainView, answerCounter);
-			ViewAsserts.assertOnScreen(mainView, answerButton);
-			ViewAsserts.assertOnScreen(mainView, commentCounter);
+			assertNotNull(mainView);
+			ViewAsserts.assertOnScreen(mainView, userListView);
+			ViewAsserts.assertOnScreen(mainView, title);
+			
+			//making sure that the title is correct
+			assertEquals("Title is not the same","F A V O R I T E S", title.getText());
+			
+			
 
 		}
-	}
-
 }
