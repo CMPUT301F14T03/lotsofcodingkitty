e571c40be0d422e3ca6b7e4b2759152306908243
rjynn <rdejesus@cs.ualberta.ca>
2014-11-06 20:08:10 -0700

    added the comments for tests and checked that the tests are properly testing what it should

--- cmput301t03appTEST-test/src/ViewCommentUIAnswerTest.java
+++ cmput301t03appTEST-test/src/ViewCommentUIAnswerTest.java
@@ -14,136 +14,171 @@ import ca.ualberta.cs.cmput301t03app.models.Question;
 import ca.ualberta.cs.cmput301t03app.views.ViewComment;
 import ca.ualberta.cs.cmput301t03app.views.ViewQuestion;
 
-// This tests the Answer mode in View comment.
-
+/**
+ * This tests that the View Comment User Interface works properly by clicking on
+ * buttons and using dialog boxes
+ * 
+ * @author rdejesus
+ * 
+ */
 public class ViewCommentUIAnswerTest extends
-		ActivityInstrumentationTestCase2<ViewComment>
-{
+
+ActivityInstrumentationTestCase2<ViewComment> {
 	Instrumentation instrumentation;
 	ActivityMonitor monitor;
 	Intent intent;
 	PostController pc;
 	Question q;
 	Comment c;
-	
-	public ViewCommentUIAnswerTest(){
+
+	public ViewCommentUIAnswerTest() {
 		super(ViewComment.class);
 	}
-	
-	public void setUp() throws Exception{
-		//just setting up the things for tests
+
+	/**
+	 * sets up everything needed for testing
+	 */
+	public void setUp() throws Exception {
+
 		super.setUp();
 		this.instrumentation = getInstrumentation();
-		this.intent=new Intent();
-		intent.putExtra(ViewQuestion.SET_COMMENT_TYPE,ViewQuestion.COMMENT_ON_ANSWER_KEY);
+		this.intent = new Intent();
+		intent.putExtra(ViewQuestion.SET_COMMENT_TYPE,
+				ViewQuestion.COMMENT_ON_ANSWER_KEY);
 		pc = new PostController(getInstrumentation().getTargetContext());
 		String qTitle = "Title";
 		String qBody = "Body";
 		String qAuthor = "Author";
-		q = new Question(qTitle,qBody,qAuthor);
+		q = new Question(qTitle, qBody, qAuthor);
 		pc.addQuestion(q);
-		c=new Comment("String","String");
-		
+		c = new Comment("String", "String");
+
 	}
-	// This tests if the view displays the correct information from the answer.
-	public void testDisplayAnswerInfo(){
-		PostController pc = new PostController(getInstrumentation().getTargetContext());
+
+	/**
+	 * This tests that the TextViews show the correct information and displays
+	 * the correct answer that is being viewed
+	 */
+
+	public void testDisplayAnswerInfo() {
+		PostController pc = new PostController(getInstrumentation()
+				.getTargetContext());
 		String qTitle = "Title";
 		String qBody = "Body";
 		String qAuthor = "Author";
 		Date date = new Date();
-		Question q1 = new Question(qTitle,qBody,qAuthor);
+		Question q1 = new Question(qTitle, qBody, qAuthor);
 		pc.addQuestion(q1);
-		
-		Answer a = new Answer("aTitle","aAuthor","1");
+
+		Answer a = new Answer("aTitle", "aAuthor", "1");
 		pc.addAnswer(a, q1.getId());
-		
+
 		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q1.getId());
 		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
-		
-		setActivityIntent(intent);
-		ViewComment vc = getActivity();
-		
-		TextView title = (TextView) vc.findViewById(R.id.comment_title);
-		TextView timeStamp = (TextView) vc.findViewById(R.id.comment_post_timestamp);
-		TextView author = (TextView) vc.findViewById(R.id.comment_post_author);
-		
-		assertEquals("Titles are not the same!",title.getText(),"Q: "+ qTitle + "\n" + "A: "+ a.getAnswer());
-		assertEquals("Authors are not the same!",author.getText(),"By: " + a.getAuthor());
-		assertEquals("Dates are not the same!",timeStamp.getText(),"Posted: " + date.toString());
-		
-	}
-	
-	// This tests if the view displays the comment count properly. 
-	// This one tests specifically if it's zero.
-	public void testDisplayQuestionCommentCountZero(){
-		
-		Answer a = new Answer("aTitle","aAuthor","1");
-		pc.addAnswer(a, q.getId());
-		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
-		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
-		
-		setActivityIntent(intent);
-		ViewComment vc = getActivity();
-		
-		TextView cc = (TextView) vc.findViewById(R.id.comment_count);
-		assertTrue("Comment count is not zero.",cc.getText().equals("Comments: 0"));
-	}
-	// Tests if the Comment Count is one.
-	public void testDisplayQuestionCommentCountOne(){
 
-		Answer a = new Answer("aTitle","aAuthor","1");
-		pc.addAnswer(a, q.getId());
-		pc.addCommentToAnswer(c, q.getId(), a.getId());
-		
-		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
-		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
-		
 		setActivityIntent(intent);
 		ViewComment vc = getActivity();
-		
+
+		TextView title = (TextView) vc.findViewById(R.id.comment_title);
+		TextView timeStamp = (TextView) vc
+				.findViewById(R.id.comment_post_timestamp);
+		TextView author = (TextView) vc.findViewById(R.id.comment_post_author);
+
+		assertEquals("Titles are not the same!", title.getText(), "Q: "
+				+ qTitle + "\n" + "A: " + a.getAnswer());
+		assertEquals("Authors are not the same!", author.getText(),
+				"By: " + a.getAuthor());
+		assertEquals("Dates are not the same!", timeStamp.getText(), "Posted: "
+				+ date.toString());
+
+	}
+
+	/**
+	 * This tests that the view is displaying the count of comments is properly
+	 * being displayed on the screen :: this tests to see that if zero comments
+	 * have been added the TextView shows zero
+	 */
+
+	public void testDisplayQuestionCommentCountZero() {
+
+		Answer a = new Answer("aTitle", "aAuthor", "1");
+		pc.addAnswer(a, q.getId());
+		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
+		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
+
+		setActivityIntent(intent);
+		ViewComment vc = getActivity();
+
 		TextView cc = (TextView) vc.findViewById(R.id.comment_count);
-		assertTrue("Comment count is not 1.",cc.getText().equals("Comments: 1"));
+		assertTrue("Comment count is not zero.",
+				cc.getText().equals("Comments: 0"));
 	}
-	
-	// This comment tests if the comments body shows correctly with only one comment.
-	public void testCommentBodyIsCorrectWithOneComment(){
-		
-		Answer a = new Answer("aTitle","aAuthor","1");
+
+	/**
+	 * This tests if a comment has been added and the comments should show one.
+	 */
+
+	public void testDisplayQuestionCommentCountOne() {
+
+		Answer a = new Answer("aTitle", "aAuthor", "1");
 		pc.addAnswer(a, q.getId());
 		pc.addCommentToAnswer(c, q.getId(), a.getId());
-		
+
 		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
 		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
-		
+
 		setActivityIntent(intent);
 		ViewComment vc = getActivity();
-		ListView lv = 
-				(ListView) vc.findViewById(R.id.commentListView);
-		
-		assertTrue("The comment body is not the same",lv.getItemAtPosition(0).equals("String"));	
+
+		TextView cc = (TextView) vc.findViewById(R.id.comment_count);
+		assertTrue("Comment count is not 1.", cc.getText()
+				.equals("Comments: 1"));
 	}
-	// Tests if the view shows two comments correctly. 
-	public void testCommentBodyIsCorretWithTwoComments(){
-		Answer a = new Answer("aTitle","aAuthor","1");
+
+	/**
+	 * this tests to see if that the correct comment is being showed properly
+	 * and the content is correct
+	 */
+	public void testCommentBodyIsCorrectWithOneComment() {
+
+		Answer a = new Answer("aTitle", "aAuthor", "1");
 		pc.addAnswer(a, q.getId());
-		Comment c1 = new Comment("String1","String1");
+		pc.addCommentToAnswer(c, q.getId(), a.getId());
+
+		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
+		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
+
+		setActivityIntent(intent);
+		ViewComment vc = getActivity();
+		ListView lv = (ListView) vc.findViewById(R.id.commentListView);
+
+		assertTrue("The comment body is not the same", lv.getItemAtPosition(0)
+				.equals("String"));
+	}
+
+	/**
+	 * tests that two comments are being shown properly and that the content is
+	 * correct.
+	 */
+	public void testCommentBodyIsCorretWithTwoComments() {
+		Answer a = new Answer("aTitle", "aAuthor", "1");
+		pc.addAnswer(a, q.getId());
+		Comment c1 = new Comment("String1", "String1");
 		pc.addCommentToAnswer(c, q.getId(), a.getId());
 		pc.addCommentToAnswer(c1, q.getId(), a.getId());
-		
+
 		intent.putExtra(ViewQuestion.QUESTION_ID_KEY, q.getId());
 		intent.putExtra(ViewQuestion.ANSWER_ID_KEY, a.getId());
-		
+
 		setActivityIntent(intent);
 		ViewComment vc = getActivity();
-		
-		ListView lv = 
-				(ListView) vc.findViewById(R.id.commentListView);
 
-		assertTrue("The comment body is not the same at position 0",lv.getItemAtPosition(0).equals("String"));
-		assertTrue("The comment body is not the same at position 1",lv.getItemAtPosition(1).equals("String1"));	
+		ListView lv = (ListView) vc.findViewById(R.id.commentListView);
+
+		assertTrue("The comment body is not the same at position 0", lv
+				.getItemAtPosition(0).equals("String"));
+		assertTrue("The comment body is not the same at position 1", lv
+				.getItemAtPosition(1).equals("String1"));
 	}
-	
-		
-		
+
 }
