0bc7dc4b2bc4d27365c1f9838b066abc693faea0
jivedesign <josh.nguyen88@gmail.com>
2014-11-14 09:52:35 -0700

    starting the question picture view stuffs

---
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/ViewPicture.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.cmput301t03app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewPicture extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
