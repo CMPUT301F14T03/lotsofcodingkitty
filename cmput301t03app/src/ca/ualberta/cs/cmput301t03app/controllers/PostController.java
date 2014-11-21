88a4b150da3c8582eef0c48d8fc3abe4f390c3bf
Theodore Brockman <tbrockma@ualberta.ca>
2014-11-20 17:00:28 -0700

    Update PostController.java

--- cmput301t03app/src/ca/ualberta/cs/cmput301t03app/controllers/PostController.java
+++ cmput301t03app/src/ca/ualberta/cs/cmput301t03app/controllers/PostController.java
@@ -2,7 +2,6 @@ package ca.ualberta.cs.cmput301t03app.controllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
